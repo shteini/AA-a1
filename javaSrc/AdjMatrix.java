4c3d4e341094d4f35e87d8f7f1b6c73407a5292f
Sam Morrison <sammorrison2@me.com>
2018-04-05 16:17:43 +1000

    Add Vertex and Add Edge completed and working correctly

--- javaSrc/AdjMatrix.java
+++ javaSrc/AdjMatrix.java
@@ -11,26 +11,29 @@ import java.util.*;
 */
 public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
 {
-  String[][] graph;
-  String[] vertexLabels;
+  int[][] graph;
+  ArrayList<String> vertexLabels;
 
 /**
  * Contructs empty graph.
  */
   public AdjMatrix() {
-    graph = new String[0][0];
+    graph = new int[0][0];
+    //Can we use Hashmap to store index and value i.e. index 0 = A
+    vertexLabels = new ArrayList<String>();
   } // end of AdjMatrix()
 
 
   public void addVertex(T vertLabel) {
+    vertexLabels.add(vertLabel.toString());
     int graphSize = graph.length;
-    String[][] temp = new String[graphSize + 1][graphSize + 1];
 
+    //Increase the size of the array by 1
+    int[][] temp = new int[graphSize + 1][graphSize + 1];
     //If there is already at least one vertex
-    if(graphSize >= 1)
+    if(graphSize > 0)
     {
-
-      //Loop through old array and add copy old values to new temp array
+      //Loop through old array and add copy old edges to new temp array
       for(int i = 0; i < graph.length; i++)
       {
         for(int j = 0; j < graph[i].length; j++)
@@ -38,52 +41,19 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
           temp[i][j] = graph[i][j];
         }
       }
-
-      //Add the new vertex
-      temp[temp.length-1][temp.length-1] = vertLabel.toString();
-      //With temp updated with the new vertex now we can save the graph
       graph = temp;
     }
     else
     {
       // No Vertices in the array
-      temp[temp.length-1][temp.length-1] = vertLabel.toString();
       graph = temp;
     }
   } // end of addVertex()
 
-
-  public void addEdge(T srcLabel, T tarLabel) {
-
-    // Check that both vertices are in the list as you can't add an edge if
-    // either of them does not exist
-
-      // temp variables to store the vertices to have an edge added.
-
-      // Loop through vertices and assign the temp variables
-      //
-        //retrieve the source and target vertex's
-
-      // add a connection to both the srclabel vertex
-      // and the tarlabel vertex as they both have their own list of connections
-      // e.g. B connected to C -> CB = 1 & BC = 1 where(1) means they are connected
-
-  } // end of addEdge()
-
-
-  public ArrayList<T> neighbours(T vertLabel) {
-      ArrayList<T> neighbours = new ArrayList<T>();
-
-      // Implement me!
-
-      return neighbours;
-  } // end of neighbours()
-
-
   public void removeVertex(T vertLabel) {
     int graphSize = graph.length;
-    String[][] temp = new String[graphSize - 1][graphSize - 1];
-
+    int[][] temp = new int[graphSize - 1][graphSize - 1];
+    vertIndex = vertexLabels.indexOf(vertLabel);
     //If there is already at least one vertex
 
       //Loop through old array and add copy old values to new temp array
@@ -94,30 +64,51 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
           //If the vertex we are looking at is not equal to the one we wish to
           //remove, we add it to temp, if it is the one we want to remove, the
           //for loop skips it and it is not added to the temp array
-          if(!(graph[i][j].equals(vertLabel)))
+          if(i != vertIndex && j != vertIndex)
           {
             temp[i][j] = graph[i][j];
           }
         }
       }
-      //With temp updated with the removed vertex now we can save the graph
+      //remove from the vertexLabel list
+      vertexLabels.remove(vertLabel);
+      //With temp updated with the removed vertex row and column now we can save the graph
       graph = temp;
 
   } // end of removeVertex()
 
+  public void addEdge(T srcLabel, T tarLabel) {
+
+    // Check that both vertices are in the list as you can't add an edge if
+    // either of them does not exist
+    if(vertexLabels.contains(srcLabel) && vertexLabels.contains(tarLabel))
+    {
+      //Get index of labels in array
+      srcIndex = vertexLabels.indexOf(srcLabel.toString());
+      tarIndex = vertexLabels.indexOf(tarlabel.toString());
+      graph[srcIndex][tarIndex] = 1;
+      graph[tarIndex][srcIndex] = 1;
+    }
+
+  } // end of addEdge()
 
   public void removeEdge(T srcLabel, T tarLabel) {
       // Implement me!
   } // end of removeEdges()
 
 
+  public ArrayList<T> neighbours(T vertLabel) {
+      ArrayList<T> neighbours = new ArrayList<T>();
+
+      // Implement me!
+
+      return neighbours;
+  } // end of neighbours()
+
   public void printVertices(PrintWriter os) {
-      for(int i = 0; i < graph.length; i++)
+      for(String vertex: vertexLabels)
       {
-        for(int j = 0; j < graph[i].length; j++)
-        {
-          os.print(graph[i][j]);
-        }
+        os.print(vertex + " ");
       }
   } // end of printVertices()
 
