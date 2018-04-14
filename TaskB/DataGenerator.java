f71d0952b8146729c189c2cad6d96f49ba059335
Sam Morrison <sammorrison2@me.com>
2018-04-14 20:22:55 +1000

    Pushing for Ilya

--- TaskB/DataGenerator.java
+++ TaskB/DataGenerator.java
@@ -1,38 +1,31 @@
 import java.io.*;
 import java.util.*;
 
-public class DataGenerator 
+public class DataGenerator
 {
-	int MAX_NUMBER = 88235;
-
-
-	//if that random number is bigger than zero and less than facebook data - random lines number
-
-	public static void main(String[] args) {
-
-		Tester t = new Tester();
-
-		ArrayList<String> data = t.getLines(500);
-		for(String line: data)
-		{
-			System.out.println(line);
-		}
-	}
-
-
-
+  int MAX_NUMBER_LINES = 88235;
   public static void main(String[] args)
   {
 
     // randomly generate one number = number of lines you are getting from facebook. Create chunk of data with this amount of lines, run everything and calculate time then return then run this method 10 times.
+    Random rand = new Random();
+
+
+    ArrayList<String> lines = Utility.getLines((rand.nextInt(MAX_NUMBER_LINES)+1));
     ScenarioOne oneAdjMat = new ScenarioOne("adjmat");
     ScenarioOne oneIndMat = new ScenarioOne("indmat");
 
     for(int i = 0; i < 10; i++)
     {
+      oneAdjMat.runScenarioOne(lines);
+      //subset i adjmat number edges, number vertexs, graph density, time it took to run the add vertex's and add edges
 
     }
 
+    for(int i = 0; i < 10; i++)
+    {
+      oneIndMat.runScenarioOne()
+    }
 
     // Now get random number to determine where to start in the file randomly 10 different sets
     // if that random number is bigger than zero and less than facebook data - random lines number
