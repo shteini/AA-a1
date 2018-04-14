12c87c5b8b41898f237e6dac7c8ed57d6860e4d3
Ilya <s3530184@student.rmit.edu.au>
2018-04-14 19:47:11 +1000

    DataGen and Tester

--- TaskB/DataGenerator.java
+++ TaskB/DataGenerator.java
@@ -1,7 +1,29 @@
-public class DataGenerator
+import java.io.*;
+import java.util.*;
+
+public class DataGenerator 
 {
+	int MAX_NUMBER = 88235;
+
+
+	//if that random number is bigger than zero and less than facebook data - random lines number
+
+	public static void main(String[] args) {
+
+		Tester t = new Tester();
+
+		ArrayList<String> data = t.getLines(500);
+		for(String line: data)
+		{
+			System.out.println(line);
+		}
+	}
+
+
 
 
 
 
 }
+
+
