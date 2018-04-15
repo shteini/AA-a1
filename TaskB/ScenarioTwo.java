fe6517711ef3cdd59bbabfd4077a8090d670c941
Ilya <s3530184@student.rmit.edu.au>
2018-04-15 14:12:18 +1000

    scenario two

--- TaskB/ScenarioTwo.java
+++ TaskB/ScenarioTwo.java
@@ -1,17 +1,64 @@
 public class ScenarioTwo
 {
-  private AdjMatrix adj;
-  private IndMatrix ind;
-  private long startTime;
-  private long finishTime;
+  private FriendshipGraph matrix;
+  private long startTimeNeighbours;
+  private long finishTimeNeighbours;
+  private long startTimeShortestPath;
+  private long finishTimeAddShortestPath;
 
-  private void runScenarioTwo(String lines)
+  public ScenarioTwo(String type)
   {
-
+    if(type.equals("adjmat"))
+    {
+      matrix = new AdjMatrix();
+    }
+    else if(type.equals("indmat"))
+    {
+      matrix = new IndMatrix();
+    }
   }
 
-  private long getTotalElapsedTime()
+  public void runScenarioTwo(ArrayList<String> lines)
   {
-    return finishTime - startTime;
+  	timeShortestPath(lines);
+  	timeNeighbours(lines);
+  }
+  
+  private void timeShortestPath(ArrayList<String> lines)
+  {
+    this.startTimeShortestPath = System.nanoTime();
+    for(String line: lines)
+    {
+      String[] vertices = line.split(" ");
+      matrix.shortestPathDistance(vertices[0], vertices[1]);
+    }
+    this.finishTimeAddShortestPath = System.nanoTime();
+  }
+
+  private void timeNeighbours(ArrayList<String> lines)
+  {
+    this.startTimeNeighbours = System.nanoTime();
+    for(String line: lines)
+    {
+      String[] vertices = line.split(" ");
+      matrix.neighbours(vertices[0]);
+      matrix.neighbours(vertices[1]);
+    }
+    this.finishTimeNeighbours = System.nanoTime();
+  }
+
+  public long getTotalElapsedTimeNeighbours()
+  {
+    return finishTimeNeighbours - startTimeNeighbours;
+  }
+
+  public long getTotalElapsedTimeShortestPath()
+  {
+    return finishTimeAddShortestPath - startTimeShortestPath;
+  }
+
+  public void printData()
+  {
+    matrix.printData();
   }
 }
